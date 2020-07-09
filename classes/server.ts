0cad75180d2008bb724c531c56454c0f3c53a1fc
Juan David Cobos <jcobos1999@gmail.com>
2020-07-08 21:41:03 -0500

    se implementación emición de mensajes

--- classes/server.ts
+++ classes/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { SERVER_PORT } from '../global/environment';
 import socketIO from 'socket.io';
 import http from 'http';
+import * as socket from '../sockets/socket';
 export default class Server{
 
     private static _instance: Server;
@@ -16,22 +17,27 @@ export default class Server{
         this.port = SERVER_PORT;
         this.httpServer = new http.Server(this.app);
         this.io = socketIO(this.httpServer);
+        this.escucharSockets();
     }
 
     public static get instance(){
         return this._instance || (this._instance = new this());
     }
 
-    private escucharSockets(){
-        console.log('escuchando conexiones');
-        this.io.on('connection', cliente =>{
-            console.log('Cliente conectado')
+
+    private escucharSockets() {
+        this.io.on('connection', cliente => {
+            console.log('Cliente conectado');
+
+            socket.mensaje(cliente, this.io);
+            socket.desconectar(cliente);
+        
         });
     }
 
 
     start( callback: any ) {
-        this.app.listen(this.port, callback);
+        this.httpServer.listen(this.port, callback);
     }
 
 }
